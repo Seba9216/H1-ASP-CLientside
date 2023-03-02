b9ca2022e854d37d9418d2925439ec5c20183a94
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-02 11:18:11 +0100

    added page

---
+++ H1-ClientSide-ASP/H1-ClientSide-ASP/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace H1_ClientSide_ASP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
